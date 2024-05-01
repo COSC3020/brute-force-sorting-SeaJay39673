77850036a18962135caa1cdee19a44b6fdcb848b
Chris <SeaJay39673>
2024-04-30 22:45:30 -0600

    FixedTestPath

--- code.test.js
+++ code.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const jsc = require('jsverify');
 
-eval(fs.readFileSync('brute-force-sorting-SeaJay39673/code.js') + '');
+eval(fs.readFileSync('code.js') + '');
 
 const test =
     jsc.forall("array nat", function (arr) {
